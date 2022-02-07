f7e0295b66bf83d6e44b039a45d3a9c782e90f6f
Linsator <Lins.kovar@gmail.com>
2022-02-07 16:49:24 +0100

    Avatar movement

--- ICP/globals.cpp
+++ ICP/globals.cpp
@@ -3,3 +3,29 @@
 
 // global variable - encapsulates all global variables
 s_globals globals;
+
+Avatar avatarMoveForward(Avatar& avatar)
+{
+	avatar.position = glm::vec3(avatar.position.x + (avatar.lookAt.x * avatar.movement_speed), avatar.position.y, avatar.position.z + (avatar.lookAt.z * avatar.movement_speed));
+	return avatar;
+}
+Avatar avatarMoveBackward(Avatar& avatar) {
+	avatar.position = glm::vec3(avatar.position.x - avatar.lookAt.x * avatar.movement_speed, avatar.position.y, avatar.position.z - avatar.lookAt.z * avatar.movement_speed);
+	return avatar;
+}
+Avatar avatarMoveLeft(Avatar& avatar) {
+	avatar.position = glm::vec3(avatar.position.x + avatar.lookAt.z * avatar.movement_speed, avatar.position.y, avatar.position.z - avatar.lookAt.x * avatar.movement_speed);
+	return avatar;
+}
+Avatar avatarMoveRight(Avatar& avatar) {
+	avatar.position = glm::vec3(avatar.position.x - avatar.lookAt.z * avatar.movement_speed, avatar.position.y, avatar.position.z + avatar.lookAt.x * avatar.movement_speed);
+	return avatar;
+}
+Avatar avatarMoveUp(Avatar& avatar) {
+	avatar.position = glm::vec3(avatar.position.x, avatar.position.y + avatar.movement_speed, avatar.position.z);
+	return avatar;
+}
+Avatar avatarMoveDown(Avatar& avatar) {
+	avatar.position = glm::vec3(avatar.position.x, avatar.position.y - avatar.movement_speed, avatar.position.z);
+	return avatar;
+}
